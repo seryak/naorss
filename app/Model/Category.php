d686c8ba7f92a7348f8c5d5150035eb90a4d9a7f
Seryak <misha-seryak@ya.ru>
2022-01-17 17:43:03 +0700

    FEAT: модели

---
+++ app/Model/Category.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Model;
+
+class Category
+{
+    protected $table = 'dle_category';
+}
---
+++ app/Model/Post.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $table = 'dle_post';
+
+}
