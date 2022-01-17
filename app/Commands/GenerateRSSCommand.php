be7413749f8c33a9eb8dadc56da5564dc3128bf7
Seryak <misha-seryak@ya.ru>
2022-01-17 19:21:04 +0700

    FIX: генератор

--- app/Commands/GenerateRSSCommand.php
+++ app/Commands/GenerateRSSCommand.php
@@ -2,6 +2,9 @@
 
 namespace App\Commands;
 
+use App\Feed\ItemDTO;
+use App\Model\Category;
+use App\Model\Post;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Facades\DB;
 use LaravelZero\Framework\Commands\Command;
@@ -13,6 +16,14 @@ class GenerateRSSCommand extends Command
 
     public function handle()
     {
+        $post = Post::find(26731);
+        $post->options;
+        dd($post->full_text);
+        $dto = new ItemDTO();
+        $dto->createFromPost($post);
+        dd($dto);
+        dd(DB::table('dle_post')->first());
+        dd($dto);
         echo DB::table('dle_post')->count();
     }
 
