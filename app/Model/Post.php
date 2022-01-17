26901ed7d05f0a84c694e1ebf14976abc96cb713
Seryak <misha-seryak@ya.ru>
2022-01-17 19:19:02 +0700

    FEAT: дополнительнные геттеры

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -2,8 +2,19 @@
 
 namespace App\Model;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property $categoryModel
+ * @property $url
+ * @property $title
+ * @property $descr
+ * @property $date
+ * @property $full_story
+ * @property array $options
+ * @property mixed $xfields
+ */
 class Post extends Model
 {
     protected $table = 'dle_post';
@@ -13,9 +24,34 @@ class Post extends Model
         return $this->hasOne(Category::class, 'id', 'category');
     }
 
-    public function getUrlAttribute()
+    public function getUrlAttribute(): string
     {
         return 'https://nao24.ru/'.$this->categoryModel->name.'/'.$this->id.'-'.$this->alt_name.'.html';
+    }
+
+    public function getOptionsAttribute(): array
+    {
+        $optionsRaw = explode('|', $this->xfields);
+        $options = [];
+        foreach ($optionsRaw as $key => $string) {
+            if (strlen(trim($string)) == 0) {
+                unset($optionsRaw[$key]);
+            }
+        }
+        $optionsRaw = array_values($optionsRaw);
+        foreach ($optionsRaw as $key => $string) {
+            if ($key % 2 == 0) {
+                $options[$string] = null;
+            } else {
+                $options[$optionsRaw[$key-1]] = $string;
+            }
+        }
+        return $options;
+    }
+
+    public function getFullTextAttribute()
+    {
+        dd($this->full_story);
     }
 
 }
